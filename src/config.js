8a6806f18f18b2c71182c4f08c7d5174fae433aa
Prayash Thapa <prayasht@gmail.com>
2017-07-28 12:54:18 -0600

    Initial scaffolding

---
+++ src/config.js
@@ -0,0 +1,19 @@
+const config = {
+  X_OFFSET: -1,
+  Y_OFFSET: 2,
+  SCALE: 1.5,
+  TEXTURES: [
+    'x',
+    'step',
+    'circle',
+    'fcircle',
+    'rect',
+    'frect',
+    'dstrips',
+    'hstrips',
+    'vstrips',
+    'dots'
+  ]
+}
+
+export default config
